64a318c59655803202223588d79b7e35a54e5b2b
Bruno Rafael Duarte de Souza <brds@cesar.org.br>
2019-10-03 17:59:28 -0300

    Reconfigure the sensor
    change configuration to lowest consumption.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# VCNL_teste
+## VCNL_teste
+
+###Important info:
+
+- 5V pull up with 4k7&Omega; on IC bus.
+- Datasheet Id is wrong. *0x51* is the correct Id.
+- Vcc is 3.3V.
+
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include<Arduino.h>
 #include <Wire.h>
 
-#define VCNL_ADDR 0x51
+#define VCNL_ADDR 0x60
 #define PS_CONF_12 0x03
+#define PS_CONF_3 0x04
 
 
 int read_sensor(char cmd){
@@ -39,7 +40,9 @@ void write_sensor(char cmd, uint16_t info){
 
 
 void start_config(){
-  write_sensor(PS_CONF_12, 0x1C02); // no interrupt, duty ratio 1/40, integrationtim 1.5T, PS ON;
+  // write_sensor(PS_CONF_12, 0x1C02); // no interrupt, duty ratio 1/40, integrationtim 1.5T, PS ON;
+  write_sensor(PS_CONF_12, 0x1CC2); // no interrupt, duty ratio 1/320, integration 1.5 T, PS 0N
+  write_sensor(PS_CONF_3, 0x0080); // reduce current consumption 1/10, sun light block off;
 }
 
 
